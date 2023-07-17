cd442711ee4fd2abf9246cad8959007cc9d172e2
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-07-17 12:28:25 +0530

    queue reversal

---
+++ Day 63/queue reversal.cpp
@@ -0,0 +1,71 @@
+/*
+Given a Queue Q containing N elements. The task is to reverse the Queue. 
+Your task is to complete the function rev(), that reverses the N elements 
+of the queue.
+
+Input:
+6
+4 3 1 10 2 6
+Output: 
+6 2 10 1 3 4
+Explanation: 
+After reversing the given elements of the queue , the resultant queue will be 6 2 10 1 3 4.
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//function Template for C++
+
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    void helper(queue <int> &q) {
+        if (q.size() == 0)
+            return;
+        int x = q.front();
+        q.pop();
+        helper(q);
+        q.push(x);
+    }
+    
+    queue<int> rev(queue<int> q)
+    {
+        helper(q);
+        
+        return q;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int test;
+    cin>>test; 
+    while(test--)
+    {
+    queue<int> q; 
+    int n, var; 
+    cin>>n; 
+    while(n--)
+    {
+        cin>>var; 
+        q.push(var);
+    }
+    Solution ob;
+    queue<int> a=ob.rev(q); 
+    while(!a.empty())
+    {
+        cout<<a.front()<<" ";
+        a.pop();
+    }
+    cout<<endl; 
+    }
+}
+// } Driver Code Ends
