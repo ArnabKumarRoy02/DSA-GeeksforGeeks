c2432f671f9d0c9e4c0e9d15068d42e5dec08ae1
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-07-17 12:47:19 +0530

    count pairs

---
+++ Day 63/count pairs with given sum.cpp
@@ -0,0 +1,57 @@
+/*
+Given an array of N integers, and an integer K, find the number of pairs of 
+elements in the array whose sum is equal to K.
+
+Input:
+N = 4, K = 6
+arr[] = {1, 5, 7, 1}
+Output: 2
+Explanation: 
+arr[0] + arr[1] = 1 + 5 = 6 
+and arr[1] + arr[3] = 5 + 1 = 6.
+*/
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int getPairsCount(int arr[], int n, int k) {
+        multiset<int> m;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int sum = k - arr[i];
+            ans += m.count(sum);
+            
+            m.insert(arr[i]);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
