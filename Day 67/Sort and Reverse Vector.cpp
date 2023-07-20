5c7aa1f4a10366612c258d714157e5e5fa8ae476
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-07-20 12:17:34 +0530

    sort and reverse vector

---
+++ Day 67/Sort and Reverse Vector.cpp
@@ -0,0 +1,73 @@
+/*
+You are given a vector V of size n. You need to sort it and reverse it.
+
+Example 1:
+
+Input:
+n = 5
+V = 1 2 3 4 5
+Output: 
+1 2 3 4 5
+5 4 3 2 1
+*/
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<int> sortVector(vector<int>v);
+vector<int> reverseVector(vector<int>v);
+
+// } Driver Code Ends
+//User function Template for C++
+
+vector<int> sortVector(vector<int>v) {
+    sort(v.begin(), v.end());
+    return v;
+}
+vector<int> reverseVector(vector<int>v)
+{
+    reverse(v.begin(), v.end());
+    return v;
+}
+
+//{ Driver Code Starts.
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    vector<int>v;
+	    for(int i=0;i<n;i++)
+	    {
+	        int x;
+	        cin>>x;
+	        v.push_back(x); //Input vector elements
+	    }
+	    
+	    v=sortVector(v); //sort and store in v
+	   
+	   for(auto x:v)
+	   cout<<x<<" "; //print elements of v
+	   cout<<endl; 
+	   v=reverseVector(v); //reverse v and store it in v
+	   for(auto x:v)
+	   cout<<x<<" "; //print elements of v
+	   cout<<endl;
+	   
+	}
+	return 0;
+}
+
+
+
+
+
+// } Driver Code Ends
