cd9a9feaa19b10aba9c30673bdac58018f7885ac
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-07-21 12:39:57 +0530

    common elements

---
+++ Day 68/Common elements.cpp
@@ -0,0 +1,72 @@
+/*
+Given three arrays sorted in increasing order. Find the elements that are 
+common in all three arrays.
+Note: can you take care of the duplicates without using any additional 
+Data Structure?
+
+Example 1:
+
+Input:
+n1 = 6; A = {1, 5, 10, 20, 40, 80}
+n2 = 5; B = {6, 7, 20, 80, 100}
+n3 = 8; C = {3, 4, 15, 20, 30, 70, 80, 120}
+Output: 20 80
+Explanation: 20 and 80 are the only
+common elements in A, B and C.
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:    
+       vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
+        {
+            vector<int> ans;
+            unordered_map<int, bool> visited;
+            for (int i = 0; i < n1; i++) {
+                if (binary_search(B, B + n2, A[i])) {
+                    if (binary_search(C, C + n3, A[i])) {
+                        if (!visited[A[i]]) {
+                            ans.push_back(A[i]);
+                            visited[A[i]] = true;
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+
+};
+
+//{ Driver Code Starts.
+
+int main ()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n1, n2, n3; 
+        cin >> n1 >> n2 >> n3;
+        int A[n1];
+        int B[n2];
+        int C[n3];
+        
+        for (int i = 0; i < n1; i++) cin >> A[i];
+        for (int i = 0; i < n2; i++) cin >> B[i];
+        for (int i = 0; i < n3; i++) cin >> C[i];
+        
+        Solution ob;
+        
+        vector <int> res = ob.commonElements (A, B, C, n1, n2, n3);
+        if (res.size () == 0) 
+            cout << -1;
+        for (int i = 0; i < res.size (); i++) 
+            cout << res[i] << " "; 
+        cout << endl;
+    }
+}
+// } Driver Code Ends
