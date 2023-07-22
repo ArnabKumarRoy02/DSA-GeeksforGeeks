91257884fa2c3ff5245a666cd3e3f017e6eb6ac8
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-07-22 10:43:35 +0530

    remove duplicates

---
+++ Day 69/Remove duplicates from an unsorted linked list.cpp
@@ -0,0 +1,133 @@
+/*
+Given an unsorted linked list of N nodes. The task is to remove duplicate 
+elements from this unsorted Linked List. When a value appears in multiple 
+nodes, the node which appeared first should be kept, all others duplicates 
+are to be removed.
+
+Example 1:
+
+Input:
+N = 4
+value[] = {5,2,2,4}
+Output: 5 2 4
+Explanation:Given linked list elements are
+5->2->2->4, in which 2 is repeated only.
+So, we will delete the extra repeated
+elements 2 from the linked list and the
+resultant linked list will contain 5->2->4
+Example 2:
+
+Input:
+N = 5
+value[] = {2,2,2,2,2}
+Output: 2
+Explanation:Given linked list elements are
+2->2->2->2->2, in which 2 is repeated. So,
+we will delete the extra repeated elements
+2 from the linked list and the resultant
+linked list will contain only 2.
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void print(Node *root)
+{
+Node *temp = root;
+while(temp!=NULL)
+{
+cout<<temp->data<<" ";
+temp=temp->next;
+}
+}
+
+
+// } Driver Code Ends
+/*
+The structure of linked list is the following
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to remove duplicates from unsorted linked list.
+    Node * removeDuplicates( Node *head) {
+        unordered_map<int, int> mp;
+        Node* curr = head;
+        while (curr != NULL) {
+            mp[curr->data]++;
+            curr = curr->next;
+        }
+        curr = head;
+        Node* temp = new Node(-1);
+        Node* prev = temp;
+        
+        while (curr) {
+            auto it = mp.find(curr->data);
+            if (it != mp.end()) {
+                Node* a = new Node(it->first);
+                prev->next = a;
+                prev = a;
+                mp.erase(it);
+            }
+            curr = curr->next;
+        }
+        return temp->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+ 
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+	    Solution ob;
+		Node *result  = ob.removeDuplicates(head);
+		print(result);
+		cout<<endl;
+		
+	}
+	return 0;
+}
+// } Driver Code Ends
