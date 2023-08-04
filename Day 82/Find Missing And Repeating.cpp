c52c95854895cb59d2991e6e6a7ffb92d289097f
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-08-04 23:11:46 +0530

    Create Find Missing And Repeating.cpp

---
+++ Day 82/Find Missing And Repeating.cpp
@@ -0,0 +1,76 @@
+/*
+Given an unsorted array Arr of size N of positive integers. One number 'A' from set {1, 2,....,N} is missing and one number 'B' occurs twice in array. Find these two numbers.
+
+Example 1:
+
+Input:
+N = 2
+Arr[] = {2, 2}
+Output: 2 1
+Explanation: Repeating number is 2 and 
+smallest positive missing number is 1.
+Example 2:
+
+Input:
+N = 3
+Arr[] = {1, 3, 3}
+Output: 3 2
+Explanation: Repeating number is 3 and 
+smallest positive missing number is 2.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function findTwoElement() which takes the array of integers arr and n as parameters and returns an array of integers of size 2 denoting the answer ( The first index contains B and second index contains A.)
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+2 ≤ N ≤ 105
+1 ≤ Arr[i] ≤ N
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    vector<int> findTwoElement(vector<int> arr, int n) {
+        int hash[n + 1] = {0};
+        
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]]++;
+        }
+        
+        int miss = -1, repeat = -1;
+        for (int i = 1; i < n + 1; i++) {
+            if (hash[i] == 2) repeat = i;
+            else if (hash[i] == 0) miss = i;
+            
+            if (repeat != -1 && miss != -1) {
+                break;
+            }
+        }
+        return {repeat, miss};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        auto ans = ob.findTwoElement(a, n);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
