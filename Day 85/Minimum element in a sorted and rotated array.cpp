f9ab6c3caa09f1e3fd9ad63998ef82f418f9f0a4
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-08-07 18:47:54 +0530

    Create Minimum element in a sorted and rotated array.cpp

---
+++ Day 85/Minimum element in a sorted and rotated array.cpp
@@ -0,0 +1,65 @@
+/*
+A sorted(in ascending order) array A[ ] with distinct elements is rotated at some unknown point, the task is to find the minimum element in it.
+
+Example 1
+
+Input:
+N = 5
+arr[] = {4 ,5 ,1 ,2 ,3}
+Output: 1
+Explanation: 1 is the minimum element inthe array.
+Example 2
+
+Input:
+N = 7
+arr[] = {10, 20, 30, 40, 50, 5, 7}
+Output: 5
+Explanation: Here 5 is the minimum element.
+ 
+
+Your Task:
+Complete the function findMin() which takes an array arr[] and n, size of the array as input parameters, and returns the minimum element of the array.
+
+Expected Time Complexity: O(log N).
+Expected Auxiliary Space: O(log N).
+
+Constraints:
+1 ≤ N ≤ 100000
+1 ≤ A[i] ≤ 1000000
+*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int findMin(int arr[], int n){
+        sort(arr, arr + n);
+        
+        return arr[0];
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int i,a[n];
+		for(i=0;i<n;i++)
+		{
+			cin>>a[i];
+		}
+		Solution ob;
+		cout<<ob.findMin(a, n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
