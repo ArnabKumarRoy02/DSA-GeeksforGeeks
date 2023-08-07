a99ffbf97ff3a3e05d09ff6eb572123abe5fa9e2
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-08-07 22:13:34 +0530

    Create Max sum in the configuration.cpp

---
+++ Day 85/Max sum in the configuration.cpp
@@ -0,0 +1,72 @@
+/*
+Given an array(0-based indexing), you have to find the max sum of i*A[i] where A[i] is the element at index i in the array. The only operation allowed is to rotate(clock-wise or counter clock-wise) the array any number of times.
+
+Example 1:
+
+Input:
+N = 4
+A[] = {8,3,1,2}
+Output: 29
+Explanation: Above the configuration
+possible by rotating elements are
+3 1 2 8 here sum is 3*0+1*1+2*2+8*3 = 29
+1 2 8 3 here sum is 1*0+2*1+8*2+3*3 = 27
+2 8 3 1 here sum is 2*0+8*1+3*2+1*3 = 17
+8 3 1 2 here sum is 8*0+3*1+1*2+2*3 = 11
+Here the max sum is 29 
+Your Task:
+Your task is to complete the function max_sum which takes two arguments which is the array A [ ] and its size and returns an integer value denoting the required max sum.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1<=N<=104
+1<=A[]<1000
+*/
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int max_sum(int A[],int N);
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        int A[N];
+        for(int i=0;i<N;i++)
+        {
+            cin>>A[i];
+        }
+
+        cout<<max_sum(A,N)<<endl;
+        /*keeping track of the total sum of the array*/
+
+    }
+}
+
+// } Driver Code Ends
+
+
+/*You are required to complete this method*/
+int max_sum(int arr[],int n) {
+    int ans = 0,sum = 0,currSum = 0;
+    
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        currSum+=arr[i]*i;
+    }
+    
+    ans=currSum;
+    for(int i = 1; i < n; i++){
+         int newSum = currSum - (sum - arr[i-1]) + arr[i-1] * (n-1);
+         currSum = newSum;
+         ans = max(ans,currSum);
+    }
+    return ans;
+}
