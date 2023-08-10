688d3b144209db7065139b8290af4afe99612b84
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-08-10 22:38:22 +0530

    Create Smallest Positive Integer that can not be represented as Sum.cpp

---
+++ Day 88/Smallest Positive Integer that can not be represented as Sum.cpp
@@ -0,0 +1,89 @@
+/*
+Given an array of size N, find the smallest positive integer value that is either not presented in the array or cannot be represented as a sum(coz sum means you are adding two or more elements) of some elements from the array.
+Example 1:
+
+Input: 
+N = 6
+array[] = {1, 10, 3, 11, 6, 15}
+Output: 
+2
+Explanation:
+2 is the smallest integer value that cannot 
+be represented as sum of elements from the array.
+Example 2:
+
+Input: 
+N = 3
+array[] = {1, 1, 1}
+Output: 
+4
+Explanation: 
+1 is present in the array. 
+2 can be created by combining two 1s.
+3 can be created by combining three 1s.
+4 is the smallest integer value that cannot be 
+represented as sum of elements from the array.
+
+Your Task:  
+You don't need to read input or print anything. Complete the function smallestpositive() which takes the array and N as input parameters and returns the smallest positive integer value that cannot be represented as the sum of some elements from the array.
+
+
+Expected Time Complexity: O(N * Log(N))
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ array[i] ≤ 109
+The array may contain duplicates.
+*/
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{   
+public:
+    long long smallestpositive(vector<long long> array, int n)
+    { 
+        long long sum = 0;
+        
+        sort(array.begin(), array.end());
+        for (int i = 0; i < n; i++) {
+            if (array[i] > sum + 1) return sum + 1;
+            sum += array[i];
+        }
+        
+        return sum + 1;
+    } 
+};
+
+
+//{ Driver Code Starts.
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<long long> array(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin>>array[i];
+        }
+        Solution ob;
+        cout<<ob.smallestpositive(array,n)<<"\n";
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
