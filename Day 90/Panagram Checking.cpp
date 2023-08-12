9df3e216f01be2b66b36369e17b1f8824cb33f67
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-08-12 11:17:51 +0530

    Create Panagram Checking.cpp

---
+++ Day 90/Panagram Checking.cpp
@@ -0,0 +1,89 @@
+/*
+Given a string check if it is Panagram or not. A panagram is a sentence containing every letter in the English Alphabet (either lowercase or uppercase or both). For example, we say the letter A is present in the string if either 'a' is present or 'A' is present.
+
+Example 1:
+
+Input:
+S = Bawds jog, flick quartz, vex nymph
+Output: 1
+Explanation: In the given input, there
+are all the letters of the English
+alphabet. Hence, the output is 1.
+Example 2:
+
+Input:
+S = sdfs
+Output: 0
+Explanation: In the given input, there
+aren't all the letters present in the
+English alphabet. Hence, the output
+is 0.
+Your Task:
+ You need to complete the function checkPangram() that takes a string as a parameter and returns true if the string is a panagram, or else it returns false.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Number of distinct characters).
+
+Constraints:
+1 ≤ |S| ≤ 104
+*/
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to check if a string is Pangram or not.
+    bool checkPangram (string &str) {
+        map<char, int> mp;
+        
+        for (int i = 0; i < str.size(); i++) {
+            if (isalpha(str[i])) {
+                mp[tolower(str[i])]++;
+            }
+        }
+        
+        int count = 0;
+        for (auto i : mp) {
+            if (i.first >= 'a' && i.first <= 'z') {
+                count++;
+            }
+        }
+        
+        if (count == 26) {
+            return true;
+        }
+        
+        return false;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+// Driver Program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--){
+        string str;
+        getline(cin, str);
+        Solution obj;
+        if (obj.checkPangram(str) == true)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+    return(0);
+}
+
+// } Driver Code Ends
