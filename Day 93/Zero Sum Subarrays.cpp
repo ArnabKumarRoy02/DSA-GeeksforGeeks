2dec82d75174a9d0421c0539a6f28b9395e73f24
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-08-15 21:20:46 +0530

    Create Zero Sum Subarrays.cpp

---
+++ Day 93/Zero Sum Subarrays.cpp
@@ -0,0 +1,83 @@
+/*
+You are given an array arr[] of size n. Find the total count of sub-arrays having their sum equal to 0.
+
+
+Example 1:
+
+Input:
+n = 6
+arr[] = {0,0,5,5,0,0}
+Output: 6
+Explanation: The 6 subarrays are 
+[0], [0], [0], [0], [0,0], and [0,0].
+
+Example 2:
+
+Input:
+n = 10
+arr[] = {6,-1,-3,4,-2,2,4,6,-12,-7}
+Output: 4
+Explanation: The 4 subarrays are [-1 -3 4]
+[-2 2], [2 4 6 -12], and [-1 -3 4 -2 2]
+
+Your Task:
+You don't need to read input or print anything. Complete the function findSubarray() that takes the array arr and its size n as input parameters and returns the total number of sub-arrays with 0 sum. 
+ 
+
+Expected Time Complexity: O(n*log(n))
+Expected Auxilliary Space: O(n)
+ 
+
+Constraints:    
+1 <= n <= 10^6
+-10^9 <= arr[ i ] <= 10^9
+*/
+//{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    //Function to count subarrays with sum equal to 0.
+    long long int findSubarray(vector<long long int> &arr, int n ) {
+        long long int result = 0;
+        map<long long int, int> freq;
+        long long int sum = 0;
+        
+        freq[0]++;
+        for (int i = 1; i <= n; i++) {
+            sum += arr[i - 1];
+            freq[sum]++;
+            result += freq[sum] - 1;
+        }
+        
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n; //input size of array
+       
+        vector<long long int> arr(n,0);
+        
+        for(int i=0;i<n;i++)
+            cin>>arr[i]; //input array elements
+        Solution ob;
+        cout << ob.findSubarray(arr,n) << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
