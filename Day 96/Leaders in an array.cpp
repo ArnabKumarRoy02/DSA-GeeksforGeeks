cab760fb144ab9d591d3e372d6ac94ed37e3dc81
Arnab Kumar Roy <86621483+ArnabKumarRoy02@users.noreply.github.com>
2023-08-18 09:54:35 +0530

    Create Leaders in an array.cpp

---
+++ Day 96/Leaders in an array.cpp
@@ -0,0 +1,97 @@
+/*
+Given an array A of positive integers. Your task is to find the leaders in the array. An element of array is leader if it is greater than or equal to all the elements to its right side. The rightmost element is always a leader. 
+
+Example 1:
+
+Input:
+n = 6
+A[] = {16,17,4,3,5,2}
+Output: 17 5 2
+Explanation: The first leader is 17 
+as it is greater than all the elements
+to its right.  Similarly, the next 
+leader is 5. The right most element 
+is always a leader so it is also 
+included.
+Example 2:
+
+Input:
+n = 5
+A[] = {1,2,3,4,0}
+Output: 4 0
+Explanation: 0 is the rightmost element
+and 4 is the only element which is greater
+than all the elements to its right.
+Your Task:
+You don't need to read input or print anything. The task is to complete the function leader() which takes array A and n as input parameters and returns an array of leaders in order of their appearance.
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(n)
+
+Constraints:
+1 <= n <= 107
+0 <= Ai <= 107
+*/
+
+//{ Driver Code Starts
+// C++ program to remove recurring digits from
+// a given number
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+    //Function to find the leaders in the array.
+    public:
+    vector<int> leaders(int a[], int n) {
+        int max = a[n - 1];
+        vector<int> res;
+        
+        res.push_back(max);
+        for (int i = n - 2; i >= 0; i--) {
+            if (a[i] < max)
+            continue;
+            max = a[i];
+            res.push_back(max);
+        }
+        
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   long long t;
+   cin >> t;//testcases
+   while (t--)
+   {
+       long long n;
+       cin >> n;//total size of array
+        
+        int a[n];
+        
+        //inserting elements in the array
+        for(long long i =0;i<n;i++){
+            cin >> a[i];
+        }
+        Solution obj;
+        //calling leaders() function
+        vector<int> v = obj.leaders(a, n);
+        
+        //printing elements of the vector
+        for(auto it = v.begin();it!=v.end();it++){
+            cout << *it << " ";
+        }
+        
+        cout << endl;
+
+   }
+}
+
+// } Driver Code Ends
